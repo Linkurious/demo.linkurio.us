0d3bc4274fe173d4742149cb6c0e09db78faffb3
davidrapin <david.rapin@gmail.com>
2015-08-27 16:27:49 +0200

    # fixes on seamless login (keep existing session if any)

--- lk.js
+++ lk.js
@@ -1,6 +1,8 @@
 
 var lk = (function() {
   var self = {};
+  //var demoUrl = 'http://crunchbase.linkurio.us/';
+  var demoUrl = 'http://192.168.1.6:3000/';
 
   if (typeof $ === 'undefined') throw new Error('"jQuery" is not declared');
   if (typeof chance === 'undefined') throw new Error('"chance" is not declared');
@@ -16,30 +18,22 @@ var lk = (function() {
     self.email = self.getUrlVar('u') || chance.email();
   };
 
-  self.register = function (button) {
-    var demoUrl = 'http://crunchbase.linkurio.us/';
-
-    var wrappedButton = $('#registerBtn');
-    var buttonText = wrappedButton.html();
+  /**
+   * @param {HTMLButtonElement} button
+   * @param {object} wrappedButton jquery wrapped button
+   * @param {function} beforeDemoOpen
+   * @param {function} failCb
+   */
+  self.doLogin = function(button, wrappedButton, beforeDemoOpen, failCb) {
     button.form.action = demoUrl + 'api/auth/loginRedirect';
 
-    wrappedButton.addClass('disabled');
-    wrappedButton.html('Creating account...');
     self.generateEmail();
-
     console.log('created fake email: ' + self.email);
 
-    wrappedButton.html('Login as "' + self.email + '" ...');
+    wrappedButton.html('Connecting as "' + self.email + '" ...');
     $('input#usernameOrEmail').val(self.email);
     $('input#password').val('demo');
     $('input#path').val('/dashboard');
-
-    var fail = function fail(step, errorData, message) {
-      wrappedButton.removeClass('disabled');
-      wrappedButton.html(buttonText);
-      console.log(step + ' error: ' + JSON.stringify(errorData, null, ' '));
-      alert(message + '\nPlease let us know at contact@linkurio.us');
-    };
 
     // enable cross-domain (work in progress)
     $.support.cors = true;
@@ -48,6 +42,7 @@ var lk = (function() {
       options.xhrFields = {withCredentials: true};
     });
 
+    // does this even work?
     function resetCookie() {
       document.cookie = 'connect.sid=; path=/; expires=Thu, 01-Jan-70 00:00:01 GMT;';
     }
@@ -63,7 +58,7 @@ var lk = (function() {
       contentType: 'application/json',
       dataType: "json"
     }).fail(function (data) {
-      fail('admin login', data, "Unable to connect to the demo.");
+      failCb('admin login', data, "Unable to connect to the demo.");
     }).done(function() {
       console.log('logged in');
 
@@ -76,28 +71,55 @@ var lk = (function() {
         processData: false,
         contentType: 'application/json'
       }).fail(function (data) {
-        fail('user create', data, "Could not create your demo account.");
+        failCb('user create', data, "Could not create your demo account.");
       }).done(function() {
         console.log('demo account created');
 
         // log out the admin
         $.ajax(demoUrl + 'api/auth/logout').fail(function (data) {
-          fail('admin logout', data, "Something went wrong while creating your demo account.");
+          failCb('admin logout', data, "Something went wrong while creating your demo account.");
         }).done(function() {
           console.log('logged out');
           resetCookie();
 
-          $('#registerBtn').html('Loading the demo ...');
-
-          $(window.parent.document.getElementById('login-header')).hide();
-          $(window.parent.document.getElementById('demo-header')).show();
-
+          beforeDemoOpen();
           button.form.submit();
         });
       });
     });
   };
 
+  self.register = function (button) {
+    var wrappedButton = $('#registerBtn');
+    var buttonText = wrappedButton.html();
+
+    wrappedButton.addClass('disabled');
+    wrappedButton.html('Checking demo session...');
+
+    var fail = function fail(step, errorData, message) {
+      wrappedButton.removeClass('disabled');
+      wrappedButton.html(buttonText);
+      console.log(step + ' error: ' + JSON.stringify(errorData, null, ' '));
+      alert(message + '\nPlease let us know at contact@linkurio.us');
+    };
+
+    var beforeDemoOpen = function() {
+      wrappedButton.html('Loading the demo ...');
+      $(window.parent.document.getElementById('login-header')).hide();
+      $(window.parent.document.getElementById('demo-header')).show();
+    };
+
+    $.ajax(demoUrl + 'api/auth/me').always(function(data, type) {
+      if (type === 'error') {
+        // not logged in
+        self.doLogin(button, wrappedButton, beforeDemoOpen, fail);
+      } else {
+        console.log('already logged in the demo');
+        beforeDemoOpen();
+        document.location.href = demoUrl;
+      }
+    });
+  };
+
   return self;
 })();
-
